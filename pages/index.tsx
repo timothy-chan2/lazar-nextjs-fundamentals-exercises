369de1bc33a062b92a364eb1f4b11880a9675099
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2023-09-17 22:50:11 -0400

    Add a title and description to the Head component of the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,9 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Fundamental Next.js</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="og:title" content="Completing Lazar's 14 fundamental Next.js exercises" key="title" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
