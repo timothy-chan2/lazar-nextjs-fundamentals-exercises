921e46d94c1cdd2fb7ef978b484df05a8d403fd6
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2023-09-19 15:52:52 -0400

    Move the global inline style into the global stylesheet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app'
 
+import '../styles.css'
+
 // The following lines of code is used to override the default App component
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component title {...pageProps} />
