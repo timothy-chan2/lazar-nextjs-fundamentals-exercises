e76165d7b5ca845c14ab12c1f86591777682a553
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2023-09-23 21:31:49 -0400

    Configure Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,19 @@ import type { AppProps } from 'next/app'
 
 import Layout from '../src/components/layout'
 
+import { ChakraProvider } from '@chakra-ui/react'
+
 import '../styles.css'
 
 // The following lines of code is used to override the default App component
 // Added a custom layout that includes a footer on all pages
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Layout>
-      <Component title {...pageProps} />
-    </Layout>
+    <ChakraProvider>
+      <Layout>
+        <Component title {...pageProps} />
+      </Layout>
+    </ChakraProvider>
   )
 }
 
